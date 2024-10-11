ae7028fca58e99ca324d2e22cfb681fd203a0a4f
Ekaterina Zarubina <zarubina.e4@wb.ru>
2024-10-11 18:03:41 +0700

    feature: add PaymentPurpose

--- src/main/java/TestData/PaymentPurpose.java
+++ src/main/java/TestData/PaymentPurpose.java
@@ -2,9 +2,8 @@ package TestData;
 
 public enum PaymentPurpose {
     REPLRNISHMENT_BORROWERS_BALANCE("Пополнение  баланса заемщика НДС не облагается\n"),
-    USER("Комиссия банка"),
-    GUEST("Поступление по займам выданным");
-
+    BANK_COMISSION("Комиссия банка"),
+    RECEIPT_OF_ISSUED_LOANS("Поступление по займам выданным");
 
     private final String description;
 
